adfce009b4bfd8ee358c891069e8d7959d9d2fbc
MutumaDerick <pdmutuma@gmail.com>
2024-07-08 13:28:37 +0300

    Added new files

--- plp_ecommerce/models.py
+++ plp_ecommerce/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='products/')
+    Availabilty = models.BooleanField(default=True)
 
-    class Details(models.model):
-        pass
+    def __str__(self):
+        return self.name
+    
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length= 60)
+    last_name = models.CharField(max_length= 60)
+    email = models.EmailField(unique=True) 
+    phone = models.CharField(max_length=20, blank=True, null=True)   
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email}"
+
+    
 
     #MVu(appurl)proj(url)T ...MVT
     #add more fields here
--- plp_ecommerce/views.py
+++ plp_ecommerce/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_list_or_404
+from .models import Customer, Product
 
 # Create your views here.
 def product_list(request):
@@ -9,5 +9,24 @@ def product_list(request):
     }
     return render(request,'plp_ecommerce/product_list.html',context)
 
-def product_details(request):
-    pass
+def product_detail(request, pk):
+    Product = get_list_or_404(Product, pk=pk)
+    context = {
+        'product': Product
+    }
+    return render(request, 'plp_ecommerce/product_detail.html', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers': customers
+    }
+    return render(request, 'plp_ecommerce/customer_list.html', context)
+
+def customer_detail(request, pk):
+    customer = get_list_or_404(Customer, pk=pk)
+    context = {
+        'customer': customer
+    }
+    return render(request,'plp_ecommerce/customer_detail.html', context)
+    
